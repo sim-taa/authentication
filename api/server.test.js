002ed88bae6141f4c6e5362861a6937bf0cf89f3
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-06-28 21:30:02 -0700

    complete test two

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,8 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+
+test("important work", () => {
+  expect(false).toBe(false);
+});
