4b29ecfaa204f2175583de1a9c6c5e532ed75561
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-07-26 11:10:38 -0700

    cleans up some console logs

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -10,24 +10,11 @@ module.exports = (req, res, next) => {
 
   jwt.verify(token, JWT_SECRET.jwtSecret, (err, decodedToken) => {
     if (err) {
-      console.log("start", err);
       return res.status(401).json({ message: "token invalid" });
     } else {
-      console.log(decodedToken);
       req.decodedToken = decodedToken;
       next();
     }
   });
   next();
-  /*
-    IMPLEMENT
-
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
 };
